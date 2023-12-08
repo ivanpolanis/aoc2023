60f854a31b4762e217d2dcf3a341116b514c1356
Ivan Polanis <polanisivan@gmail.com>
2023-12-08 10:09:52 -0300

    day 2 - part2

---
+++ day-2/src/bin/part2.rs
@@ -0,0 +1,64 @@
+struct Game {
+    id: usize,
+    red: u32,
+    blue: u32,
+    green: u32,
+}
+
+impl Game {
+    fn new(id: usize) -> Game {
+        return Game {
+            id,
+            red: 0,
+            blue: 0,
+            green: 0,
+        };
+    }
+}
+
+fn main() -> Result<(), std::num::ParseIntError> {
+    let file = include_str!("input.txt");
+
+    let mut count = 0;
+
+    for (i, line) in file.lines().enumerate() {
+        let mut card = Game::new(i + 1);
+        let game = line.split(": ").last().unwrap();
+        let rounds = game.split("; ");
+        for round in rounds {
+            let data = round.split(", ");
+            for j in data {
+                let mut splitted = j.split_ascii_whitespace();
+                let qty: u32 = match splitted.next().unwrap().parse() {
+                    Ok(n) => n,
+                    Err(_e) => std::process::exit(1),
+                };
+                let color = splitted.next().unwrap();
+
+                match color {
+                    "red" => {
+                        if card.red < qty {
+                            card.red = qty
+                        }
+                    }
+                    "blue" => {
+                        if card.blue < qty {
+                            card.blue = qty
+                        }
+                    }
+                    "green" => {
+                        if card.green < qty {
+                            card.green = qty
+                        }
+                    }
+                    _ => std::process::exit(1),
+                }
+            }
+        }
+        count += card.red * card.blue * card.green;
+    }
+
+    println!("{count}");
+
+    Ok(())
+}
